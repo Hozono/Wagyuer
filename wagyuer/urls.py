25f0bb323139f4ac964a89c5f440be3ee0e8f015
ChihiroHozono <chihiro4434132@gmail.com>
2021-01-24 10:23:03 +0900

    fix: urls.py

--- wagyuer/urls.py
+++ wagyuer/urls.py
@@ -1,23 +1,18 @@
 from django.urls import include, path
 
 from .api_urls import package_router
-from .views import IndexView, WagyuInfomationView
+from .views import IndexView
 from .api_views import WagyuInfomationAPIView
 
 app_name = "wagyuer"
 
-api_urlpatterns = [  # apiのURL一覧
-    path("packages/", include(package_router.urls)),  # 慣例として複数形にする
+api_urlpatterns = [
+    path("packages/", include(package_router.urls)),
     path("wagyuId/", WagyuInfomationAPIView.as_view()),
 ]
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
-    path(
-        "wagyu_infomation/<int:pk>",
-        WagyuInfomationView.as_view(),
-        name="wagyu_infomation",
-    ),
     path(
         "api/0.1/",
         include(api_urlpatterns),
